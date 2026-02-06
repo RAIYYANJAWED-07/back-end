d52c15e4405ff35be2c0dea2caa416b9172c2041
RAIYYANJAWED-07 <raiyyanjawed786@gmail.com>
2026-02-06 18:39:52 +0530

    day-11

--- day-08/Backend/src/app.js
+++ day-08/Backend/src/app.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const path = require('path')
 
 const app = express()
-
 const notesModel = require('./models/notes.model')
 
 app.use(cors())
@@ -73,8 +71,5 @@ app.patch('/api/notes/:id', async (req,res)=>{
     })
 })
 
-app.use('*name' , (req,res)=>{
-    res.sendFile(path.join(__dirname,'..','/public/index.html'))
-})
 
 module.exports = app
